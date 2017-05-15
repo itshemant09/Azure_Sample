02fb4fa750b9e4d5c80eeda8b9f1b2c43fcb2089
itshemant09 <itshemant09@gmail.com>
2017-05-16 03:16:30 +0530

    NA

--- Azure_Learning_sample_code/Program.cs
+++ Azure_Learning_sample_code/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace Azure_Learning_sample_code
 {
@@ -10,6 +13,44 @@ namespace Azure_Learning_sample_code
     {
         static void Main(string[] args)
         {
+            CloudStorageAccount csa = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            CloudBlobClient blobclient = csa.CreateCloudBlobClient();
+            CloudBlobContainer blobcontainer = blobclient.GetContainerReference("blobcontainer");
+            blobcontainer.CreateIfNotExists();
+            blobcontainer.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+
+            //Download
+
+
+            
+            
+            
+            //Upload
+
+
+            //foreach(IListBlobItem item in blobcontainer.ListBlobs(null,false))
+            //{
+            //    if(item.GetType()==typeof(CloudBlockBlob))
+            //    {
+            //        CloudBlockBlob cbb = (CloudBlockBlob)item;
+            //        Console.WriteLine("block blob length {0} {1}", cbb.Properties.Length, cbb.Uri);
+            //    }
+            //    else if(item.GetType()==typeof(CloudPageBlob))
+            //    {
+            //        CloudPageBlob cpb = (CloudPageBlob)item;
+            //        Console.WriteLine("page blob length {0} {1}", cpb.Properties.Length, cpb.Uri);
+            //    }
+            //    else if (item.GetType() == typeof(CloudBlobDirectory))
+            //    {
+            //        CloudBlobDirectory cbd = (CloudBlobDirectory)item;
+            //        Console.WriteLine("page blob length  {1}", cbd.Uri);
+            //    }
+            //}
+            //CloudBlockBlob blockblob = blobcontainer.GetBlockBlobReference("Myblob");
+            // using (var filestream = System.IO.File.OpenRead(@"C:\AzureSample\demo.txt"))
+            //{
+            //    blockblob.UploadFromStream(filestream);
+            //}
         }
     }
 }
