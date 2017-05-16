d05bd8078fa866d0f58f924dddf449a511d9c973
itshemant09 <itshemant09@gmail.com>
2017-05-17 02:58:56 +0530

    h

--- QueueStoage/Program.cs
+++ QueueStoage/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace QueueStoage
 {
@@ -10,6 +13,48 @@ namespace QueueStoage
     {
         static void Main(string[] args)
         {
+            //conneting to storage accunt
+            CloudStorageAccount csa = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("samplecodeazure"));
+
+            //creating queue client
+            CloudQueueClient clientqueue = csa.CreateCloudQueueClient();
+
+            //creatng queue
+            CloudQueue queue = clientqueue.GetQueueReference("myqueue");
+
+            //create queue if not exist
+            queue.CreateIfNotExists();
+
+            //insert message into queue
+            CloudQueueMessage cqm = new CloudQueueMessage("Hi, There! testing");
+            queue.AddMessage(cqm);
+
+            //Peek message
+            CloudQueueMessage cqm1 = queue.PeekMessage();
+            Console.WriteLine(cqm1.AsString);
+
+            //Update Queue message
+            CloudQueueMessage cqm2 = queue.GetMessage();
+            cqm2.SetMessageContent("Updated message");
+            queue.UpdateMessage(cqm2, TimeSpan.FromSeconds(60.0), MessageUpdateFields.Content | MessageUpdateFields.Visibility);
+
+            //Delete message
+           // CloudQueueMessage cqm3 = queue.GetMessage();
+           // queue.DeleteMessage(cqm3);
+
+            //take all message from queue
+            foreach (CloudQueueMessage msg in queue.GetMessages(20, TimeSpan.FromMinutes(5)))
+            {
+                Console.WriteLine(msg.AsString);
+            }
+
+            //get length
+            queue.FetchAttributes();
+            int? contentmsg = queue.ApproximateMessageCount;
+            Console.WriteLine(contentmsg);
+
+            //Delete queue
+            queue.Delete();
         }
     }
 }
