c54e2858d68ee885c6d15e37abb7c144d122b050
itshemant09 <itshemant09@gmail.com>
2017-05-20 03:16:05 +0530

    j

---
+++ FileStorage/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.File;
+
+namespace FileStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating stoarge acount on azure
+
+            CloudStorageAccount csa = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+        }
+    }
+}
